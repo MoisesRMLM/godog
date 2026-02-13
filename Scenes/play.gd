f7bbdc46632d0452fc81f5542cd576da072c8417
mengualbarragandavid-wq <mengualbarragandavid@gmail.com>
2026-02-13 08:52:48 +0100

    script play

---
+++ Scenes/play.gd
@@ -0,0 +1,10 @@
+extends Button
+
+@export var level: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pressed.connect(_play)
+
+func _play():
+	get_tree().change_scene_to_packed(level)
