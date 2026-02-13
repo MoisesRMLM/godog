4a33f686664c0f7f7f6ee5a7edc65ef1eb341f58
María González Bermúdez <mikalemx@gmail.com>
2026-02-13 10:33:36 +0100

    pause menu

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,9 +1,8 @@
 extends Control
 
 
-func _on_play_pressed() -> void:
-	pass 
-	# get_tree().change_scene_to_file("res://Scenes/game.tscn")
+func _on_play_pressed() -> void: 
+	get_tree().change_scene_to_file("res://Scenes/map.tscn")
 
 
 func _on_options_pressed() -> void:
